29fccc5efcbfcb2dde2899fa75a26460a7031e34
sujinabiju <sujina.biju@gmail.com>
2017-09-27 16:48:55 +0530

    Halfway done with new feature.Tested so far, so good.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function  something(){
-	'Im something';
+function aBigNewFeature(){
+	console.log('writing ablote of code');
+	console.log('Halfway done with this new feature');
 }
+
+function something(){
+	console.log('Im something');
+}
+
 console.log('hello world');
 something();
+aBigNewFeature();
+
+
+
+
